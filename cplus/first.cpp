3dec665232ee7735a537625259df9663348c503a
George <cd5160866@126.com>
2020-04-28 08:39:41 +0800

    add for c++

---
+++ cplus/first.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std ;
+//
+// Created by 覃伟 on 2020/4/28.
+//
+
+void test1();
+
+int main(){
+    test1();
+    return 0;
+}
+
+void test1(){
+    std::cout << "Hello, World!" << std::endl;
+}
